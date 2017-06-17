1d89b592b481c36941d5c0384188e8842571e78c
Hano <hinsul3113@gmail.com>
2017-06-17 14:34:06 -0700

    Added Program, organization, age group, program type (#3)

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,8 +3,11 @@ class Program < ApplicationRecord
   belongs_to :program_type
   belongs_to :organization
 
+
+
   validates :description, { presence: {message: "must be provided"}}
   validates :registration, inclusion: { in: ['registered', 'drop in'] }
   validates :cost, inclusion: { in: ['free', 'paid'] }
   
+
 end
