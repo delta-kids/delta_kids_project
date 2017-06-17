1178cdf171931a832cbba135d235b0e73784e49d
Hung Nguyen <reachhungnguyen@gmail.com>
2017-06-17 10:19:29 -0700

    Add bootstrap_and_overrides && required in application.js/application.scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require chosen-jquery
+//= require welcome
 //= require_tree .
