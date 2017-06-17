89d307209e481e20f8705e491e7fdbbc69ba4276
Hano <hinsul3113@gmail.com>
2017-06-17 15:08:26 -0700

    Organization validations (#6)
    * Added validations to organizations
    
    *  silly commit
    
    * Added validation to organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,14 @@
 class Organization < ApplicationRecord
   belongs_to :age_group
+
+
+  validates :description, {presence: {message: "must be provided"}}
+  validates :name, presence: { message: 'must be provided'}, uniqueness: true
+
+  validates :phone,   :presence => {:message => 'Only positive number without spaces are allowed'},
+                     :numericality => true,
+                     :length => { :minimum => 10, :maximum => 15 }
+
+
+
 end
