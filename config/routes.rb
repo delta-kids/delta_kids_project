f12d340189a76f695c66dc7bb05362f288a97303
Carlo Villaceran <cvillaceran@gmail.com>
2017-06-18 22:41:01 -0700

    finished services page

--- config/routes.rb
+++ config/routes.rb
@@ -133,15 +133,15 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'home#contact', as: 'contact'
 
-  get '/programs/:id', to: 'programs#index'
+  get '/programs/0_5', to: 'programs#index0_5'
 
-  get '/services/pregnancy_babies/:id', to: 'services#pregnancy_babies'
+  get '/programs/6_12', to: 'programs#index6_12'
 
-  get '/services/children/:id', to: 'services#children'
+  get '/services/pregnancy_babies', to: 'services#pregnancy_babies'
 
-  get '/services', to: 'services#index'
+  get '/services/children', to: 'services#children'
 
-  get '/services/families/:id', to: 'services#families'
+  get '/services/families', to: 'services#families'
 
   get '/live', to: 'home#live', as: 'live'
 
