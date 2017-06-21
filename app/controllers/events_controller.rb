7ef6aafb62344afd03b589bdb237490dd4dbcf4f
Hano <hinsul3113@gmail.com>
2017-06-20 17:14:56 -0700

    events show, index, create and new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,48 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
 
-
-  # ALL USERS, including ADMIN
-
-  def index
-  end
 
   def new
+    @event = Event.new
   end
+
 
   def create
+    @event = Event.new event_params
+    if @event.save
+      flash[:notice] = 'Event create'
+      redirect_to event_path(@event)
+    else
+      flash.now[:alert] = @event.pretty_errors
+      render :new
+    end
   end
+
 
   def show
+    @event = Event.find(params[:id])
   end
 
-  def edit
+  def index
+    @events = Event.all
   end
 
-  def update
-  end
 
-  def destroy
+  # def edit
+  # end
+  #
+  # def update
+  # end
+  #
+  # def destroy
+  # end
+
+  private
+  def event_params
+    params.require(:event).permit([:title, :start_date, :end_date, :start_time, :event_repeat, :event_location, :address, :cost, :registration, :more_info, :contact_name, :contact_email, :approved])
+  end
+  def find_event
+    @event = Event.find params[:id]
   end
 
 end
