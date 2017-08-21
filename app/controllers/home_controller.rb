549b50356ecaddd820763f6c1627f2e5c7f0ac46
Chris Ryan <chrisryan@hotmail.ca>
2017-08-21 03:16:04 -0700

    made some major changes to the dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,6 +21,14 @@ class HomeController < ApplicationController
     # render :layout => false
   end
 
+  def pending_and_approved_events
+
+  end
+
+  def pending_and_approved_resources
+
+  end
+
   def surveys
   end
 
