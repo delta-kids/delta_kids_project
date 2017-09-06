cd1ae00602ae8c75a9be41a4fe8e6461e8cb92bd
Chris Ryan <chrisryan@hotmail.ca>
2017-09-06 01:43:06 -0700

    made partial for login msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to home_path, notice: "Thank you for signing up"
+      redirect_to dashboard_path, notice: "Thank you for signing up"
     else
       render :new
     end
