c11cb8b24b335946355fccb2b918013b7d2f640d
Chris Ryan <chrisryan@hotmail.ca>
2017-10-10 15:43:12 -0700

    alot of work done for the search and filter on calendar page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,22 @@
 class EventsController < ApplicationController
-  before_action :find_event, only: [:show, :edit, :update, :destroy]
-
   # ALL USERS, including ADMIN
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
+  has_scope :by_date, :using => [:start_date, :end_date], :type => :hash
+
+  has_scope :event_location, :type => :array
+  has_scope :registration, :type => :array
+  has_scope :cost, :type => :array
+  has_scope :age_groups, :type => :array
+  # has_scope :registration
+
 
   def index
-    @events = Event.all
-    @events_by_start_date = @events.group_by(&:start_date)
-    @search_count = Event.search(params[:term])
-    @events_search = Event.search(params[:term]).page(params[:page]).per(5)
+    # @events = Event.all
+    # @event_location = Event.where("event_location IN (?)", params[:event_location])
+    @events = apply_scopes(Event).all
+    @search_count = @events.search(params[:term])
+    @events_search = @events.search(params[:term]).page(params[:page]).per(5)
+    @events_by_start_date = @search_count.group_by(&:start_date)
   end
 
   def index2
@@ -60,7 +69,9 @@ class EventsController < ApplicationController
 
   private
   def event_params
-    params.require(:event).permit([:title, :term, :start_date, :end_date, :start_time, :end_time, :event_repeat, :event_location, :address, :cost, :registration, :more_info, :contact_name, :contact_email, :approved, :description, :image, { age_group_ids: [] } ])
+    params.require(:event).permit([:title, :term, :start_date, :end_date, :start_time, :end_time, :event_repeat, :event_location, :address, :cost, :registration, :more_info, :contact_name, :contact_email, :approved, :description, :image, { age_group_ids: [] }, { program_type_ids: [] },
+    { tag_ids: [] }
+    ])
   end
   def find_event
     @event = Event.find params[:id]
