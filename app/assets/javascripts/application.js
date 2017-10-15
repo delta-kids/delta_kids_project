d0412dcdc8181a55ea1b5d83e522caf1d65a29a5
Chris Ryan <chrisryan@hotmail.ca>
2017-10-15 14:21:50 -0700

    reverse last change, more =require gmaps to top of list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui/widgets/datepicker
@@ -18,7 +19,6 @@
 //= require chosen-jquery
 //= require rails-ujs
 //= require underscore
-//= require gmaps/google
 //= require_tree .
 
 
