2b4ce0586c0f2f578b365caa952da29f65a58988
Chris Ryan <chrisryan@hotmail.ca>
2017-10-14 20:12:17 -0700

    more bug fixing

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,13 @@ module DeltaKidsProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
