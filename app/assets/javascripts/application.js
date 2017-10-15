c1338adfe904322cfaaefe25e41479d652a0eb75
Chris Ryan <chrisryan@hotmail.ca>
2017-10-15 14:30:32 -0700

    still tying to fix gmaps not defined

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require underscore
 //= require gmaps/google
 //= require jquery
 //= require jquery_ujs
@@ -18,7 +19,6 @@
 //= require bootstrap-timepicker
 //= require chosen-jquery
 //= require rails-ujs
-//= require underscore
 //= require_tree .
 
 
