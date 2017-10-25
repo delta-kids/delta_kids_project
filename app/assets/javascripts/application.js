0dc14b30e39b4ac6383d4fb3d4c78b1563bbcd26
Chris Ryan <chrisryan@hotmail.ca>
2017-10-24 18:27:10 -0700

    filter checkboxes are now togglable via the label as well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,6 +45,22 @@ $("[href]").each(function() {
 // });
 // });
 
+let checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+    $checkboxes = $("#checkbox-container :checkbox");
+
+$checkboxes.on("change", function(){
+  $checkboxes.each(function(){
+    checkboxValues[this.id] = this.checked;
+  });
+
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+// On page load
+$.each(checkboxValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
 $(document).ready(function(){
   $('.timepicker').timepicker();
 });
