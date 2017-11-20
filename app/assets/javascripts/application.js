6f4795e27aaeb7fb450e65abe382b33d1458fe88
Chris Ryan <chrisryan@hotmail.ca>
2017-11-20 13:36:02 -0800

    testing script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,19 +51,3 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.datepicker').datepicker({ dateFormat: 'yy-mm-dd' });
 });
-
-let checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $(".checkbox-container :checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-// On page load
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
