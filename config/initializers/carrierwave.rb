a9074262f0d3e33f66760368e019ceb20f60d947
Chris Ryan <chrisryan@hotmail.ca>
2017-12-08 10:23:42 -0800

    troubleshooting aws configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :aws_access_key_id => ENV['AWSAccessKeyId'],
+    :aws_secret_access_key => ENV['AWSSecretKey'],
     :region => ENV['S3_REGION']
   }
   config.fog_directory = ENV['S3_BUCKET']
