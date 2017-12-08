998106e6f18c0cb4ce05f2fdba7e88ba4b65f0b4
Chris Ryan <chrisryan@hotmail.ca>
2017-12-07 18:10:08 -0800

    minor change to s3.rb

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,10 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'us-west-2'
+    :provider =>'AWS',
+    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :region => 'us-west-2'
   }
-  config.fog_provider = 'fog/aws'
   config.fog_directory = "delta-kids-2017"
 end
