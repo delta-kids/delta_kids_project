eca3c5d0c3e95d12f1d54b1ba18b257e0ac68757
Chris Ryan <chrisryan@hotmail.ca>
2018-02-21 13:40:30 -0800

    calendar orderby

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,10 +13,10 @@ class EventsController < ApplicationController
   has_scope :age_group, :type => :array
 
   def index
-    @events = apply_scopes(Event).all.order("start_time asc")
+    @events = apply_scopes(Event).all
     @events_search = @events.page(params[:page]).per(5)
     @events_by_start_date = @events.group_by(&:start_date)
-    @calendar_events = @events.flat_map{ |e| e.calendar_events(
+    @calendar_events = @events.order("start_time asc").flat_map{ |e| e.calendar_events(
       params.fetch(:by_start_date, Time.zone.now - 3.months ).to_date,
       params.fetch(:by_end_date, Time.zone.now + 3.months).to_date
        ) }
