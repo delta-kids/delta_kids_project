408e6007c794c268e043da8d722ee87e159ef559
Chris Ryan <chrisryan@hotmail.ca>
2018-03-01 16:29:05 -0800

    calendar issues seems to be 100% fixed locally, testing production

--- app/models/event.rb
+++ app/models/event.rb
@@ -31,7 +31,7 @@ def calendar_events(start_d, end_d)
   if recurring.empty?
       [self]
   else
-      schedule(start_d, end_d).occurrences(end_d).map do |date|
+      schedule(start_d, end_d).occurrences(self.end_time).map do |date|
           Event.new(id: id, title: title, start_time: date, end_time: date, description: description, address: address)
       end
   end
